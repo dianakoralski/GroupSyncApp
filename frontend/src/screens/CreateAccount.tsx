95feeae852374589cfc50f8198860e0bff717281
Diana Koralski <dianakoralski@hotmail.com>
2023-08-21 20:11:23 -0700

    added date picker for birthday

--- frontend/src/screens/CreateAccount.tsx
+++ frontend/src/screens/CreateAccount.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import {
   View,
   Text,
@@ -6,17 +7,49 @@ import {
   StyleSheet,
   TouchableOpacity,
   ScrollView,
+  Pressable,
+  Platform,
 } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "react-navigation-stack/lib/typescript/src/vendor/types";
 import { StackParams } from "../../App";
 import { TextInput } from "react-native-gesture-handler";
+import DateTimePicker from "@react-native-community/datetimepicker";
 import BackButton from "../components/BackButton";
 import RoundButton from "../components/RoundButton";
 
 export const CreateAccount = () => {
   const navigation = useNavigation<StackNavigationProp<StackParams>>();
+  const today = new Date();
+  const minDate = new Date(today);
+  minDate.setFullYear(today.getFullYear() - 200);
+  const [dateOfBirth, setDateofBirth] = useState("");
+  const [date, setDate] = useState(new Date());
+  const [showPicker, setShowPicker] = useState(false);
+
+  const toggleDatePicker = () => {
+    setShowPicker(!showPicker);
+  };
+
+  const onChange = ({ type }: any, selectedDate: any) => {
+    if (type === "set") {
+      const currentDate = selectedDate;
+      setDate(currentDate);
+
+      if (Platform.OS === "android") {
+        toggleDatePicker();
+        setDateofBirth(currentDate.toDateString());
+      }
+    } else {
+      toggleDatePicker();
+    }
+  };
+
+  const confirmIOSDate = () => {
+    setDateofBirth(date.toDateString());
+    toggleDatePicker();
+  };
 
   return (
     <View style={styles.container}>
@@ -34,8 +67,55 @@ export const CreateAccount = () => {
             </Text>
 
             <View style={styles.whiteBox}>
-              <Text style={styles.label}>Username:</Text>
-              <TextInput style={styles.input} placeholder="enter email here" />
+              <Text style={styles.label}>First Name:</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="enter first name here"
+              />
+              <Text style={styles.label}>Last Name:</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="enter last name here"
+              />
+              <Text style={styles.label}>Date of Birth:</Text>
+              {showPicker && (
+                <DateTimePicker
+                  mode="date"
+                  display="spinner"
+                  value={date}
+                  onChange={onChange}
+                  maximumDate={new Date()}
+                  style={{ height: "20%" }}
+                  minimumDate={minDate}
+                />
+              )}
+              {showPicker && Platform.OS === "ios" && (
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-around",
+                  }}
+                >
+                  <TouchableOpacity onPress={toggleDatePicker}>
+                    <Text>Cancel</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={confirmIOSDate}>
+                    <Text>Confirm</Text>
+                  </TouchableOpacity>
+                </View>
+              )}
+              {!showPicker && (
+                <Pressable onPress={toggleDatePicker}>
+                  <TextInput
+                    style={styles.input}
+                    placeholder="ex. August 13, 1999"
+                    value={dateOfBirth}
+                    onChangeText={setDateofBirth}
+                    editable={false}
+                    onPressIn={toggleDatePicker}
+                  />
+                </Pressable>
+              )}
               <Text style={styles.label}>Email:</Text>
               <TextInput style={styles.input} placeholder="enter email here" />
               <Text style={styles.label}>Password:</Text>
@@ -43,7 +123,6 @@ export const CreateAccount = () => {
                 style={styles.input}
                 placeholder="enter password here"
               />
-
               <TouchableOpacity
                 style={styles.button}
                 onPress={() => navigation.navigate("Home")}
