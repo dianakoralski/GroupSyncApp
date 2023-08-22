877cc66095e8685164f0e5647ddd693cd6236213
Diana Koralski <dianakoralski@hotmail.com>
2023-08-21 19:22:49 -0700

    setup backend

---
+++ backend/routes/Posts.ts
@@ -0,0 +1,16 @@
+import { express } from "../index";
+const { Posts } = require("../models");
+const router = express.Router();
+
+router.get("/", async (req: any, res: any) => {
+  const listOfPosts = await Posts.findAll();
+  res.json(listOfPosts);
+});
+
+router.post("/", async (req: any, res: any) => {
+  const post = req.body;
+  await Posts.create(post);
+  res.json(post);
+});
+
+module.exports = router;
